96dd632d0b502a7990c048a8eb441006de7d4d0a
Allan Kwek <allan.kwek.18@gmail.com>
2024-09-04 15:29:16 +0700

    tambah main

--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'npm' : '2306152134',
+        'name': 'Allan Kwek',
+        'class': 'PBP F'
     }
 
     return render(request, "main.html", context)
